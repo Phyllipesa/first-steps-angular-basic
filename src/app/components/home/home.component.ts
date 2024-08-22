1bd9675a7654095d9070c0d755f2d837a231d6bb
Phyllipe <phyllipe.sjc@hotmail.com>
2024-08-22 15:48:35 -0300

    consumindo os dados da api no component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { CommonModule } from '@angular/common';
+import { JokeService } from '../../services/joke.service';
 
 @Component({
   selector: 'app-home',
@@ -14,8 +15,21 @@ export class HomeComponent {
   count = signal(0);
   value = true;
   imgUrl = "https://d14d9vp3wdof84.cloudfront.net/image/589816272436/image_hf7nb732th2tvd4ghacvc40u76/-S265-FWEBP"
+  joke = '';
 
   lista = ["Bender", "Fry", "lila"]
+
+  constructor(private service: JokeService) {
+    this.service.getTest().subscribe(
+      {
+        next: data => this.joke = data.value,
+        error: err => console.log(err),
+        complete: () => {
+          console.log("finalizado"); 
+        }
+      }
+    )
+  }
 
   test() {
     this.count.update(value => value + 1);
